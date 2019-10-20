570d48c8bf8fe8394b072128c5545158b3f33ce3
bbsmithy <bsmith@cartrawler.com>
2019-10-20 19:28:16 +0100

    Improvements to article snippets

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -6,7 +6,9 @@ export const ArticleCard = (props: ArticleCardProps) => {
     <article className="center mw5 mw7-ns hidden br2 ba dark-gray b--black-10  mv3">
       <div className="pa3">
         <p className="f6 f5-ns lh-copy">{props.content}</p>
-        <a href={props.origin}>{props.origin}</a>
+        <a href={props.origin} className="i f6">
+          {props.origin}
+        </a>
       </div>
     </article>
   );
--- src/screens/topic-screens/ArticleSnippets.tsx
+++ src/screens/topic-screens/ArticleSnippets.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ArticleCard } from "../../components/ArticleCard";
+import { ArticleCardProps } from "../../types";
 
 const dummySnippets = [
+  {
+    origin:
+      "https://techcrunch.com/2019/10/16/evervault-raises-3-2m-from-sequoia-kleiner-for-an-api-to-build-apps-with-privacy-baked-in/",
+    content: `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+rebum.`
+  },
   {
     origin:
       "https://techcrunch.com/2019/10/16/evervault-raises-3-2m-from-sequoia-kleiner-for-an-api-to-build-apps-with-privacy-baked-in/",
@@ -37,10 +46,18 @@ rebum.`
 ];
 
 export const ArticleSnippets = (props: any) => {
-  if (true) {
+  const [snippets, setSnippets] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSnippets(dummySnippets);
+    }, 500);
+  }, []);
+
+  if (snippets.length) {
     return (
       <div className="topic-section-container">
-        {dummySnippets.map(snippet => {
+        {snippets.map(snippet => {
           return (
             <ArticleCard content={snippet.content} origin={snippet.origin} />
           );

c8fc24f9652d94469fdf81162fcdfc653c0c3fd4
bbsmithy <bsmith@cartrawler.com>
2019-12-19 13:40:31 +0000

    ImageCard styling

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,17 +1,33 @@
 import React from "react";
+import { createUseStyles } from "react-jss";
 
 type ImageCardProps = {
   src: string;
   origin: string;
 };
 
+const useStyles = createUseStyles({
+    imageContainer:{
+        textAlign: 'center'
+
+    },
+    image:{
+        width: '100%'
+    }
+})
+
 export const ImageCard = (props: ImageCardProps) => {
+
+    const classes = useStyles();
+
   return (
-    <article className="center shadow-card mw5 mw7-ns hidden br2 ba dark-gray b--black-10  mv3">
-      <div className="pa3">
-        <img src={props.src} />
-        <p>{props.origin}</p>
-      </div>
+    <article className="center shadow-card mw7 hidden br2 ba dark-gray b--black-10  mv3">
+        <div className={classes.imageContainer}>
+            <img src={props.src} className={classes.image}/>
+        </div>
+        <div className="pa3">
+            <a href={props.src}>{props.src}</a>
+        </div>
     </article>
   );
 };

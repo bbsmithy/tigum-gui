0507ad334ef0ac9835fc6602bf346772ff8ac157
bbsmithy <bsmith@cartrawler.com>
2020-01-12 00:19:31 +0000

    Splash screen

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Login } from "./Login";
 import { useEffect } from "react";
 import { checkLogin } from "./client-lib/api";
@@ -8,17 +8,27 @@ import { MainContent } from "./main-content-view";
 export const Auth = () => {
   // @ts-ignore
   const [state, dispatch] = useStateValue();
+  const [showSplash, setShowSplash] = useState(true);
 
   const isUserLoggedIn = async () => {
     try {
       const user = await checkLogin();
       dispatch({ type: "LOGIN_SUCCESS", payload: user });
+      setShowSplash(false);
     } catch (e) {}
   };
 
   useEffect(() => {
     isUserLoggedIn();
   }, []);
+
+  if (showSplash && !state.user.loggedIn) {
+    return (
+      <div style={{ margin: "auto", textAlign: "center", marginTop: "22%" }}>
+        <img src={require("./logo.png")} height={45} />
+      </div>
+    );
+  }
 
   return <>{state.user && state.user.loggedIn ? <MainContent /> : <Login />}</>;
 };

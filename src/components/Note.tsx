8d82f66c6acc19e67d783a0d43c2e933493a4502
bbsmithy <briansmith.work578@gmail.com>
2020-03-16 22:08:02 +0000

    Note card responsive improvements

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { getDate } from "../util";
+import React from 'react';
+import { getDate } from '../util';
 
 export const Note = (props: any) => {
   const navigateToNote = () => {
@@ -12,18 +12,20 @@ export const Note = (props: any) => {
   };
 
   return (
-    <div className="card note-card pointer" onClick={navigateToNote}>
-      <div className="mw9 center">
-        <div className="cf ph2-ns">
-          <div className="fl ph2 w-90 pv1">
-            <div>
-              <h4 className="note-card-title">{props.note.title}</h4>
-              <p className="note-sub-title">{renderDate()}</p>
+    <div className='fl w-50 w-50-m w-33-ns ph2 pv1'>
+      <div className='card w-100 note-card pointer' onClick={navigateToNote}>
+        <div className='mw9 center'>
+          <div className='cf ph2-ns'>
+            <div className='fl ph2 w-90 pv1'>
+              <div>
+                <h5 className='note-card-title'>{props.note.title}</h5>
+                <p className='note-sub-title'>{renderDate()}</p>
+              </div>
             </div>
-          </div>
-          <div className="fl w-10 pv4">
-            <div>
-              <i className="fas fa-chevron-right"></i>
+            <div className='fl w-10 pv4'>
+              <div>
+                <i className='fas fa-chevron-right'></i>
+              </div>
             </div>
           </div>
         </div>

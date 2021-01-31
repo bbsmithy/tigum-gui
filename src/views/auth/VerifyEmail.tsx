4daebc3a9f023417f48484d096dadf99908f0425
bbsmithy <briansmith.work578@gmail.com>
2021-01-31 16:02:56 +0000

    Improved signip/logn UI

--- src/views/auth/VerifyEmail.tsx
+++ src/views/auth/VerifyEmail.tsx
@@ -1,99 +1,48 @@
 import React, { useState } from 'react';
-import { loginUser, signupUser } from '../../clib/api';
+import { loginUser } from '../../clib/api';
 import { useStateValue } from '../../state/StateProvider';
 import { useEvervault } from '@evervault/react';
 import { createUseStyles } from 'react-jss';
-import classes from '*.module.css';
+
 
 const useStyles = createUseStyles({
   disabledBtn: {
     backgroundColor: "gray",
-    color: "#efefef",
     outline: "none",
     display: "inline",
     borderRadius: 4,
-    border: "2px solid gray",
-    width: "100%",
-    cursor: "not-allowed"
+    border: "2px solid #246bf8"
   },
   activeBtn: {
     backgroundColor: "#246bf8",
     outline: "none",
     display: "inline",
     borderRadius: 4,
-    border: "2px solid #246bf8",
-    width: "100%",
-    cursor: "pointer"
-  },
-  container: {
-    '@media (max-width: 600px)':{
-      padding: "30px 20px"
-    },
-    padding: 50
-  },
-  eyeIcon:{
-    padding: 8,
-    border: "1px solid white",
-    backgroundColor: "#246bf8",
-    textAlign: "center",
-    cursor: "pointer",
-    borderTopRightRadius: ".25rem",
-    borderBottomRightRadius: ".25rem",
-  },
-  passwordInput: {
-    borderBottomLeftRadius: ".25rem",
-    borderTopLeftRadius: ".25rem"
+    border: "2px solid #246bf8"
   }
 });
 
-const PasswordInput = ({ onChangePassword, password, classes }) => {
-  const [showPassword, setShowPassword] = useState(false)
-
-  const toggleShowPassword = () => {
-    setShowPassword(!showPassword)
-  }
-
-  return (
-    <div style={{display: "flex", flexDirection: "row"}}>
-      <div style={{ flex: 9 }}>
-        <input
-          className={`${classes.passwordInput} b white dib pa2 bc-white input-reset b--white ba bg-transparent hover-bg-black hover-white w-100`}
-          type={showPassword ? 'text' : 'password'}
-          onChange={onChangePassword}
-          value={password}
-          name='password'
-          id='password'
-        />
-      </div>
-      <div style={{ flex: 1 }}>
-        <div className={classes.eyeIcon} onClick={toggleShowPassword}>
-          {showPassword ? <i className="fas fa-eye-slash"></i> : <i className='fas fa-eye' />}
-        </div>
-      </div>
-    </div>
-  )
-}
 
 
-const LoginForm = ({ dispatch, navigateToSignup, classes }) => {
+const LoginForm = ({ dispatch, navigateToSignup }) => {
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
   const [loginError, setLoginError] = useState('');
   const [authing, setAuthing] = useState(false);
 
   const login = async () => {
-    if (isValidForm && !authing) {
-      setAuthing(true);
-      try {
-        const res = await loginUser(email, password);
-        if (res) {
-          dispatch({ type: 'LOGIN_SUCCESS', payload: res });
-          setAuthing(false);
-        }
-      } catch (e) {
+    setAuthing(true);
+    try {
+      const res = await loginUser(email, password);
+      if (res) {
+        dispatch({ type: 'LOGIN_SUCCESS', payload: res });
         setAuthing(false);
-        setLoginError('Incorrect email or password');
       }
+    } catch (e) {
+      setAuthing(false);
+      setLoginError('Incorrect email or password');
     }
   };
 
@@ -104,8 +53,6 @@ const LoginForm = ({ dispatch, navigateToSignup, classes }) => {
   const onChangePassword = (e) => {
     setPassword(e.target.value);
   };
-
-  const isValidForm = email && password
 
   return (
     <>
@@ -123,7 +70,14 @@ const LoginForm = ({ dispatch, navigateToSignup, classes }) => {
         </div>
         <div className='mv3 white'>
           <label className='db fw6 lh-copy f6 mb1'>Enter Password</label>
-          <PasswordInput onChangePassword={onChangePassword} password={password} classes={classes} />
+          <input
+            className='b white br2 pa2 bc-white input-reset b--white ba bg-transparent hover-bg-black hover-white w-100'
+            type='password'
+            onChange={onChangePassword}
+            value={password}
+            name='password'
+            id='password'
+          />
         </div>
       </fieldset>
       {loginError && (
@@ -132,10 +86,16 @@ const LoginForm = ({ dispatch, navigateToSignup, classes }) => {
         </div>
       )}
       <div className='white'>
+        <span
+          onClick={navigateToSignup}
+          className='link underline white fl pointer'
+        >
+          I don't have an account yet
+        </span>
         <button
-          className={`${isValidForm ? classes.activeBtn : classes.disabledBtn} b ph3 pv2 br2 input-reset ba b--white bg-transparent white f6`}
+          className='b ph3 pv2 fr br2 input-reset ba b--white bg-transparent hover-bg-black hover-white white pointer f6 dib'
           type='submit'
-          disabled={!isValidForm || authing}
+          disabled={authing}
           onClick={login}
         >
           {authing ? (
@@ -146,39 +106,33 @@ const LoginForm = ({ dispatch, navigateToSignup, classes }) => {
             'Login'
           )}
         </button>
-        <div
-          onClick={navigateToSignup}
-          className={`link underline tc mt4 white pointer`}
-        >
-          I don't have an account yet
-        </div>
       </div>
     </>
   )
 }
 
 
-const SignupForm = ({ dispatch, navigateToSignup, classes }) => {
+const SignupForm = ({ dispatch, navigateToSignup }) => {
 
+  const classes = useStyles()
   const evervault = useEvervault();
+
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+
   const [loginError, setLoginError] = useState('');
   const [authing, setAuthing] = useState(false);
 
   const onClickSignUp = async () => {
-    if (isValidForm && !authing) {
-      setAuthing(true)
-      try {
-        setAuthing(true)
-        const encrypted = await evervault.encrypt({ email })
-        await signupUser(name, encrypted.email, password)
-        setAuthing(false)
-      } catch (e) {
-        setAuthing(false)
-        setLoginError('Failed to create account')
-      }
+    try {
+      const encrypted = await evervault.encrypt({ email, password })
+      // const decrypted = await evervault.run("tigum-signup-cage", encrypted)
+      console.log(name, encrypted)
+    } catch (e) {
+      console.log(e)
+      setAuthing(false)
+      setLoginError('Failed to create account')
     }
   };
 
@@ -194,12 +148,12 @@ const SignupForm = ({ dispatch, navigateToSignup, classes }) => {
     setPassword(e.target.value);
   };
 
-  const isValidForm = email && password && name
+  const isValidForm = email && name && password
 
 
   return (
     <>
-      <fieldset id='sign_up' className='ba b--transparent ph0 mh1'>
+      <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
         <div className='mt3 white'>
           <label className='db fw6 lh-copy f6 mb1'>Username</label>
           <input
@@ -220,7 +174,12 @@ const SignupForm = ({ dispatch, navigateToSignup, classes }) => {
         </div>
         <div className='mv3 white'>
           <label className='db fw6 lh-copy f6 mb1'>New Password</label>
-          <PasswordInput onChangePassword={onChangePassword} password={password} classes={classes} />
+          <input
+            className='b white br2 pa2 bc-white input-reset b--white ba bg-transparent hover-bg-black hover-white w-100'
+            type='password'
+            onChange={onChangePassword}
+            value={password}
+          />
         </div>
       </fieldset>
       {loginError && (
@@ -228,11 +187,17 @@ const SignupForm = ({ dispatch, navigateToSignup, classes }) => {
           <p>{loginError}</p>
         </div>
       )}
-      <div className='white center'>
+      <div className='white'>
+        <span
+          onClick={navigateToSignup}
+          className='link underline white fl pointer'
+        >
+          I already have an account
+        </span>
         <button
-          className={`${isValidForm ? classes.activeBtn : classes.disabledBtn} b ph3 pv2 br2 input-reset ba b--white bg-transparent white f6`}
+          className={`${isValidForm ? classes.activeBtn : classes.disabledBtn} b ph3 pv2 fr br2 input-reset ba b--white bg-transparent white pointer f6 dib`}
           type='submit'
-          disabled={!isValidForm || authing}
+          disabled={authing}
           onClick={onClickSignUp}
         >
           {authing ? (
@@ -243,25 +208,15 @@ const SignupForm = ({ dispatch, navigateToSignup, classes }) => {
             'Sign Up'
           )}
         </button>
-        <div
-          onClick={navigateToSignup}
-          className='link underline tc mt4 white pointer'
-        >
-          I already have an account login
-        </div>
       </div>
     </>
   )
 }
 
 export const Login = (props) => {
-
-
   // @ts-ignore
   const [state, dispatch] = useStateValue();
   const [showLogin, setShowLogin] = useState(true)
-
-  const classes = useStyles()
 
 
   const navigateToSignup = () => {
@@ -269,13 +224,13 @@ export const Login = (props) => {
   };
 
   return (
-    <main className={`white ${classes.container}`}>
-      <div className='measure center'>
+    <main className='pa5 white' >
+      <div className='measure center shadow-2' style={{backgroundColor: "#333", padding: "30px 20px", height: 480, borderRadius: 20}}>
         <img src={require('../../logo-tigum.png')} className='w-33' />
         <p>Your personal knowledge base for the web</p>
         {showLogin ? 
-          <LoginForm dispatch={dispatch} navigateToSignup={navigateToSignup} classes={classes} /> :
-          <SignupForm dispatch={dispatch} navigateToSignup={navigateToSignup} classes={classes} />
+          <LoginForm dispatch={dispatch} navigateToSignup={navigateToSignup} /> :
+          <SignupForm dispatch={dispatch} navigateToSignup={navigateToSignup} />
         }   
       </div>
     </main>

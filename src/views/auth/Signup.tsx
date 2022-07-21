ba41f1f3602d18ec99ed971199a1d867b96d33a4
bbsmithy <briansmith.work578@gmail.com>
2022-07-21 15:12:43 +0100

    Added signup deeplink email/name

--- src/views/auth/Signup.tsx
+++ src/views/auth/Signup.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { signupUser } from '../../clib/api';
-import { useStateValue } from '../../state/StateProvider';
-import { useEvervault } from '@evervault/react';
-import { createUseStyles } from 'react-jss';
-import { goto } from '../../util';
-import LoginCardResult from '../../components/LoginResultCard';
+import React, { useState } from "react";
+import { signupUser } from "../../clib/api";
+import { useStateValue } from "../../state/StateProvider";
+import { useEvervault } from "@evervault/react";
+import { createUseStyles } from "react-jss";
+import { goto } from "../../util";
+import LoginCardResult from "../../components/LoginResultCard";
 
 const useStyles = createUseStyles({
   disabledBtn: {
@@ -15,7 +15,7 @@ const useStyles = createUseStyles({
     borderRadius: 4,
     border: "2px solid gray",
     width: "100%",
-    cursor: "not-allowed"
+    cursor: "not-allowed",
   },
   activeBtn: {
     backgroundColor: "#246bf8",
@@ -24,15 +24,15 @@ const useStyles = createUseStyles({
     borderRadius: 4,
     border: "2px solid #246bf8",
     width: "100%",
-    cursor: "pointer"
+    cursor: "pointer",
   },
   container: {
-    '@media (max-width: 600px)':{
-      padding: "30px 20px"
+    "@media (max-width: 600px)": {
+      padding: "30px 20px",
     },
-    padding: 50
+    padding: 50,
   },
-  eyeIcon:{
+  eyeIcon: {
     padding: 8,
     border: "1px solid white",
     backgroundColor: "#246bf8",
@@ -43,61 +43,68 @@ const useStyles = createUseStyles({
   },
   passwordInput: {
     borderBottomLeftRadius: ".25rem",
-    borderTopLeftRadius: ".25rem"
-  }
+    borderTopLeftRadius: ".25rem",
+  },
 });
 
 const PasswordInput = ({ onChangePassword, password, classes }) => {
-  const [showPassword, setShowPassword] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
 
   const toggleShowPassword = () => {
-    setShowPassword(!showPassword)
-  }
+    setShowPassword(!showPassword);
+  };
 
   return (
-    <div style={{display: "flex", flexDirection: "row"}}>
+    <div style={{ display: "flex", flexDirection: "row" }}>
       <div style={{ flex: 9 }}>
         <input
           className={`${classes.passwordInput} b white dib pa2 bc-white input-reset b--white ba bg-transparent hover-bg-black hover-white w-100`}
-          type={showPassword ? 'text' : 'password'}
+          type={showPassword ? "text" : "password"}
           onChange={onChangePassword}
           value={password}
-          name='password'
-          id='password'
+          name="password"
+          id="password"
         />
       </div>
       <div style={{ flex: 1 }}>
         <div className={classes.eyeIcon} onClick={toggleShowPassword}>
-          {showPassword ? <i className="fas fa-eye-slash"></i> : <i className='fas fa-eye' />}
+          {showPassword ? (
+            <i className="fas fa-eye-slash"></i>
+          ) : (
+            <i className="fas fa-eye" />
+          )}
         </div>
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 const SignupForm = ({ dispatch, classes }) => {
-
+  const params = new URLSearchParams(window.location.search);
   const evervault = useEvervault();
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const [signupError, setSignupError] = useState('');
-  const [signupSuccess, setSignupSuccess] = useState('')
+  const [email, setEmail] = useState(params.get("email"));
+  const [name, setName] = useState(params.get("name"));
+  const [password, setPassword] = useState("");
+  const [signupError, setSignupError] = useState("");
+  const [signupSuccess, setSignupSuccess] = useState("");
   const [authing, setAuthing] = useState(false);
 
   const onClickSignUp = async () => {
     if (isValidForm && !authing) {
-      setAuthing(true)
+      setAuthing(true);
       try {
-        setAuthing(true)
-        const encrypted = await evervault.encrypt({ email })
-        await signupUser(name, email, encrypted.email, password)
-        setSignupSuccess('Thanks for signing up! Check your email for your account verification link')
-        setAuthing(false)
+        setAuthing(true);
+        const encrypted = await evervault.encrypt({ email });
+        await signupUser(name, email, encrypted.email, password);
+        setSignupSuccess(
+          "Thanks for signing up! Check your email for your account verification link"
+        );
+        setAuthing(false);
       } catch (e) {
-        setAuthing(false)
-        setSignupError('Failed to create account, contact briansmith.work578@gmail.com for support')
+        setAuthing(false);
+        setSignupError(
+          "Failed to create account, contact briansmith.work578@gmail.com for support"
+        );
       }
     }
   };
@@ -106,7 +113,7 @@ const SignupForm = ({ dispatch, classes }) => {
     setEmail(e.target.value);
   };
 
-  const onChangeName= (e) => {
+  const onChangeName = (e) => {
     setName(e.target.value);
   };
 
@@ -114,80 +121,97 @@ const SignupForm = ({ dispatch, classes }) => {
     setPassword(e.target.value);
   };
 
-  const isValidForm = email && password && name
-
+  const isValidForm = email && password && name;
 
   return (
     <>
-      <fieldset id='sign_up' className='ba b--transparent ph0'>
-        <div className='mt3 white'>
-          <label className='db fw6 lh-copy f6 mb1'>Username</label>
+      <fieldset id="sign_up" className="ba b--transparent ph0">
+        <div className="mt3 white">
+          <label className="db fw6 lh-copy f6 mb1">Username</label>
           <input
-            className='pa2 white br2 input-reset ba bg-transparent b--white hover-bg-black hover-white w-100'
+            className="pa2 white br2 input-reset ba bg-transparent b--white hover-bg-black hover-white w-100"
             type="text"
             onChange={onChangeName}
             value={name}
           />
         </div>
-        <div className='mt3 white'>
-          <label className='db fw6 lh-copy f6 mb1'>New Email</label>
+        <div className="mt3 white">
+          <label className="db fw6 lh-copy f6 mb1">Email</label>
           <input
-            className='pa2 white br2 input-reset ba bg-transparent b--white hover-bg-black hover-white w-100'
-            type='email'
+            className="pa2 white br2 input-reset ba bg-transparent b--white hover-bg-black hover-white w-100"
+            type="email"
             onChange={onChangeEmail}
             value={email}
           />
         </div>
-        <div className='mv3 white'>
-          <label className='db fw6 lh-copy f6 mb1'>New Password</label>
-          <PasswordInput onChangePassword={onChangePassword} password={password} classes={classes} />
+        <div className="mv3 white">
+          <label className="db fw6 lh-copy f6 mb1">Password</label>
+          <PasswordInput
+            onChangePassword={onChangePassword}
+            password={password}
+            classes={classes}
+          />
         </div>
       </fieldset>
-      {signupError && (
-        <LoginCardResult type="ERROR" message={signupError} />
-      )}
+      {signupError && <LoginCardResult type="ERROR" message={signupError} />}
       {signupSuccess && (
         <LoginCardResult type="SUCCESS" message={signupSuccess} />
       )}
-      <div className='white center'>
+      <div className="white center">
         <button
-          className={`${isValidForm ? classes.activeBtn : classes.disabledBtn} b ph3 pv2 br2 input-reset ba b--white bg-transparent white f6`}
-          type='submit'
+          className={`${
+            isValidForm ? classes.activeBtn : classes.disabledBtn
+          } b ph3 pv2 br2 input-reset ba b--white bg-transparent white f6`}
+          type="submit"
           disabled={!isValidForm || authing}
           onClick={onClickSignUp}
         >
           {authing ? (
             <div>
-              <i className='fas fa-circle-notch fa-spin'></i> Computing...
+              <i className="fas fa-circle-notch fa-spin"></i> Beep Bop Boop...
             </div>
           ) : (
-            'Sign Up'
+            "Sign Up"
           )}
         </button>
         <div
           onClick={() => {
             goto(`login`);
           }}
-          className='link underline tc mt4 white pointer'
+          className="link underline tc mt4 white pointer"
         >
           I already have an account login
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export const Signup = (props) => {
   // @ts-ignore
   const [state, dispatch] = useStateValue();
-  const classes = useStyles()
+  const [stepNumber, setStepNumber] = useState(1);
+  const classes = useStyles();
 
   return (
     <main className={`white ${classes.container}`}>
-      <div className='measure center'>
-        <img src={require('../../logo-tigum.png')} className='w-33' />
-        <p>Your personal learning enviroment for the web</p>
-        <SignupForm dispatch={dispatch} classes={classes} />
+      <div className="measure center">
+        <img src={require("../../logo-tigum.png")} className="w-33" />
+        <p>Your study tool for the web</p>
+        <div
+          style={{
+            boxShadow: "0 3px 6px rgb(0 0 0 / 16%), 0 3px 6px rgb(0 0 0 / 23%)",
+            background: "#333",
+            borderRadius: 12,
+            padding: "20px 20px 30px 20px",
+          }}
+        >
+          <i>Step {stepNumber} of 2</i>
+          {stepNumber === 1 && (
+            <SignupForm dispatch={dispatch} classes={classes} />
+          )}
+          {stepNumber === 2 && <div>HOWYA</div>}
+        </div>
       </div>
     </main>
   );
